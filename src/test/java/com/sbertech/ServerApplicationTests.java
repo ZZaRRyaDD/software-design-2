bcf72909170826b7d7dd83369a7ed57450ec132c
Nazar <furnoxl@yandex.ru>
2025-02-09 12:46:19 +0300

    init commit

---
+++ src/test/java/com/sbertech/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbertech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
